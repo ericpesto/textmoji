3fe06e24a2182e86b06ab0d70c94967c57abaca9
Eric Petsopoulos <epetsopoulos@gmail.com>
2022-07-23 23:41:19 +0100

    split text into string array

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [textPhrase, setTextPhrase] = useState<string>('')
+  const [textPhraseWords, setTextPhraseWords] = useState<string[]>([])
+  const [uncodePhraseWords, setUnicodePhraseWords] =  useState<string[]>([])
+  const [emojiPhraseWords, setEmojiPhraseWords] = useState<string[]>([])
+  const [emojiPhrase, setEmojiPhrase] = useState<string>('')
+
+  const handleTextInput = (e: any) => {
+    // bug/ account for extra spaces? atm they are counted as words
+    setTextPhrase( e.target.value)
+  }
+
+
+  const splitTextPhraseIntoWords = () => {
+    const phraseToWords = textPhrase.split(' ')
+    setTextPhraseWords(phraseToWords)
+    console.log(textPhraseWords)
+  }
+
+  const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleTextInput(e)
+    splitTextPhraseIntoWords()
+  }
+
+  // need a better way to account for copy and paste
+  // do i need a useEffect?
+
+  console.log('textPhrase ->', textPhrase)
+  console.log('textPhraseWords ->', textPhraseWords)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Translate text to 🙂
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" value={textPhrase} onChange={handleUserInput}></input>
+        <p>Text: {textPhrase}</p>
+        <p>Words:</p>
+        {textPhraseWords.map((word, index) => {
+          return <li key={index}>{word}</li>
+        })}
       </header>
     </div>
   );
--- todo.md
+++ todo.md
@@ -1,8 +1,9 @@
 Function: convert phrases of text into emojis, where possible, if not keep word in. 
 
-0// store all enoji names in an array
+
 1// get string of test
 2// split string at spaces into array
+0// store all enoji names in an array
 3// filter through array
 4// if item in array = a emoji meta name, add the necessary symbol before to make sure it is displayed as an emoji
 
@@ -16,3 +17,17 @@ conversion is, it finds word match with emoji name, convert that name to its UTF
 Converting unicode to emoji resources:
 - https://stackoverflow.com/questions/51640509/what-is-the-correct-way-to-convert-unicode-to-emoji
 
+
+EXEMPT:
+``` 
+Emoji is a subset of unicode. There is no conversion from unicode to emoji necessary or possible. Just change your array to
+
+var data = ["\u{1F923}", "\u{1F603}"]
+If your input is a hex number, you can use
+
+String.fromCodePoint(parseInt ("1F923", 16))
+In HTML you can also use HTML hex entities
+
+"&#x" + "1F923" + ";"
+
+```
